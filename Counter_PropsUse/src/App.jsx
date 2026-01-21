8bb02e63bf3b4b27bcf8696b9d687ad4bdd59bc9
AkashMondal27 <akashmondal102003@gmail.com>
2026-01-21 11:11:45 +0530

    refactor: adjust indentation and formatting in App component

--- Counter_PropsUse/src/App.jsx
+++ Counter_PropsUse/src/App.jsx
@@ -2,27 +2,27 @@ import { useState } from 'react'
 import './App.css'
 import Button from './assets/Button';
 
-function App() {
-  const [count, setCount] = useState(0);
+    function App() {
+      const [count, setCount] = useState(0);
 
-   function handleClick() {
-    setCount(count + 1);
-  }
-  // reset Function
- function handleReset() {
-    setCount(0);
-  }
-  return (
-    <>
-      <div>
-        <Button
-         handleClick={handleClick}
-         handleReset={handleReset} 
-         />
-      </div>
-      <p>Count: {count}</p>
-    </>
-  )
-}
+      function handleClick() {
+        setCount(count + 1);
+      }
+      // reset Function
+        function handleReset() {
+            setCount(0);
+          }
+          return (
+            <>
+              <div>
+                <Button
+                handleClick={handleClick}
+                handleReset={handleReset} 
+                />
+              </div>
+              <p>Count: {count}</p>
+            </>
+          )
+    }
 
-export default App
+    export default App
--- State_Lifting/src/App.jsx
+++ State_Lifting/src/App.jsx
@@ -3,23 +3,23 @@ import './App.css'
 import Card from './components/card'
 
 
-function App() {
+  function App() {
 
-  // 1st, ceate a state in parent component
-    const[name , setName ]=useState("");
-  return (
-    <>
-      <div>
-      {/* 2nd ,create a Card component & pass the state and setState as props to child component */}
-       <Card name={name} setName={setName} />
-      
-       {/* 4th , display the value of name state in parent component */}
-       <p>I am inside Parent Component and Value of name is {name}</p>
-      
-      </div>
-      
-    </>
-  )
-}
+    // 1st, ceate a state in parent component
+      const[name , setName ]=useState("");
+    return (
+      <>
+        <div>
+        {/* 2nd ,create a Card component & pass the state and setState as props to child component */}
+        <Card name={name} setName={setName} />
+        
+        {/* 4th , display the value of name state in parent component */}
+        <p>I am inside Parent Component and Value of name is {name}</p>
+        
+        </div>
+        
+      </>
+    )
+  }
 
 export default App

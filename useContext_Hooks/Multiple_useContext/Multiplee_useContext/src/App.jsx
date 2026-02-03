e8033784404c01cbfce877f0af6f4f6040cfed95
AkashMondal27 <akashmondal102003@gmail.com>
2026-02-03 13:03:45 +0530

    use of multiple useContext hooks

--- useContext_Hooks/Multiple_useContext/Multiplee_useContext/src/App.jsx
+++ useContext_Hooks/Multiple_useContext/Multiplee_useContext/src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createContext, useState } from 'react'
+
 import './App.css'
 import ChildA from './Conponents/ChildA'
 
+const ThemeContext = createContext();
+const UserContext = createContext();
+
 function App() {
-  const [count, setCount] = useState(0)
+      const [theme, setTheme] = useState('light');
+      const [user, setUser] = useState({name: "AKASH MONDAL", mobile: "1234567890", })
 
   return (
     <>
-      <div>
-         <ChildA/>
-      </div>
+     <UserContext.Provider value={user}>
+      <ThemeContext.Provider value={{theme, setTheme}}>
+         <div id="Container" 
+         style={{backgroundColor: theme === 'light' ? 'white' : 'black', }}>  
+          <ChildA/>
+        </div>
+      </ThemeContext.Provider>
+      </UserContext.Provider>
     </>
   )
 }
 
 export default App
+export { ThemeContext, UserContext };
--- useContext_Hooks/Multiple_useContext/Multiplee_useContext/src/Conponents/ChildC.jsx
+++ useContext_Hooks/Multiple_useContext/Multiplee_useContext/src/Conponents/ChildC.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useContext } from 'react';
+import { ThemeContext, UserContext } from '../App';
 
 const ChildC = () => {
+
+    const user= useContext(UserContext);
+    const {theme, setTheme} = useContext(ThemeContext);
+     function handleClick (){
+        if(theme=== 'light'){
+            setTheme('dark')
+        }else
+            setTheme('light')
+     }
+
+
   return (
     <div>
-      <h1>good morning</h1>
+     <button onClick={handleClick }>
+        Click Me
+        </button> <br />
+        User
+        data : {user.name }
     </div>
   )
 }

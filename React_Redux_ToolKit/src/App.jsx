4045c087328cd4d2fb1d05c161150080ca957908
AkashMondal27 <akashmondal102003@gmail.com>
2026-02-09 10:07:45 +0530

    react redux toolkit code

--- React_Redux_ToolKit/src/App.jsx
+++ React_Redux_ToolKit/src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment ,incrementByAmount,reset } from './features/counter/CounterSlice';
+import { useState } from 'react';
+ 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount , setAmount]=useState("");
 
+  const count= useSelector((state)=> state.counter.value);
+  const dispatch= useDispatch();
+
+  function handleIncrementClick (){
+    dispatch(increment());
+
+  }
+  function handleDecrementClick(){
+    dispatch(decrement());
+  }
+
+  function handleResetClick(){
+    dispatch(reset());
+    setAmount("");
+  }
+
+  function handleIncAmountClick(){
+    dispatch (incrementByAmount( amount));
+  }
   return (
     <>
-      <div>
-        <h1> Once Again Baics Structure</h1>
+      <div className="container">
+        <button onClick={ handleIncrementClick}> +</button>
+        <p> Counter : {count}</p>
+        <button onClick={handleDecrementClick}> -</button>
+        <input 
+         type='Number'
+         value={amount}
+         placeholder='Enter the value '
+         onChange={(e)=>setAmount(e.target.value)} />
+         <button className=' amount' onClick={handleIncAmountClick}>
+            Increment<br/> by amount
+         </button>
+         <button className=' reset' onClick={handleResetClick}>
+            Reset
+         </button>
       </div>
       
     </>

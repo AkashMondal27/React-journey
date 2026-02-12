122f1419e84d075379a60357d99ae42ba05ad4f3
AkashMondal27 <akashmondal102003@gmail.com>
2026-02-12 10:05:49 +0530

    1st example for useMemo_Hook

--- useMemo/React_useMemo1/src/App.jsx
+++ useMemo/React_useMemo1/src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useMemo} from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
+  const[input, setInput]=useState(0);
+  // making it expensive as it need more time to run
+  function expensiveText(num){
+    console.log("make expensive text");
+    for(let i=0;i<=10000000; i++){}  
+    return num*2;
+  }
+  // let doublevalue = expensiveText(input); 
+  let doublevalue= useMemo(() => expensiveText(input), [input])
+  return ( 
     <>
       <div>
-       <h1> Use of useMemo</h1>
+       <button onClick={()=>setCount(count +1)}>
+        Increment 
+       </button>
       </div>
-      
+      <div>
+        Count :{count}
+      </div>
+
+      <div>
+        Double :{doublevalue}
+      </div>
+      <input 
+        type='number'
+        placeholder='enter a number '
+        value={input}
+        onChange={(e)=> setInput(e.target.value)}
+        >
+      </input>
     </>
   )
 }

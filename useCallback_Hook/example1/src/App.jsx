e3c8aa99b26d047cb7cef356fe00f332cad6b4f2
AkashMondal27 <akashmondal102003@gmail.com>
2026-02-14 21:42:10 +0530

    recode

--- useCallback_Hook/example1/src/App.jsx
+++ useCallback_Hook/example1/src/App.jsx
@@ -2,14 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ChildComponent from './components/childComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleClick(){
+    setCount(count +1);
+  }
+
   return (
     <>
       <div>
-        <h1> UseCallback Hook</h1>
+        <div>
+          count:{count}
+        </div> 
+        <br/>
+        <div>
+          <button onClick={handleClick}>Increment </button>
+        </div> 
+        <br/>
+        <br/>
+        <div>
+          <ChildComponent buttonName= 'click me' />
+        </div>
       </div>
      
     </>
